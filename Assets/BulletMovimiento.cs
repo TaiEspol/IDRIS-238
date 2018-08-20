5376212c8df233ec02bce26e59b3cee8c28c7a04
juniverjair <jjroman@espol.edu.ec>
2018-08-19 21:24:32 -0500

    enemigo estatico dispara con la tecla e

---
+++ Assets/BulletMovimiento.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovimiento : MonoBehaviour {
+
+	private Rigidbody2D bulletRB;
+	public float bulletSpeed;
+	public float bulletLife;
+
+	void Awake(){
+		bulletRB = GetComponent<Rigidbody2D>();
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		bulletRB.velocity = new Vector2(bulletSpeed, bulletRB.velocity.y);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Destroy(gameObject, bulletLife);
+		
+	}
+}
---
+++ Assets/Enemigo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour {
+
+	public Transform bulletSpawner;
+	public GameObject bulletPrefab;
+
+	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		EnemyShooting();
+	}
+
+	public void EnemyShooting(){
+		if (Input.GetKey("e"))
+		{
+			Instantiate(bulletPrefab, bulletSpawner.position, bulletSpawner.rotation);
+		}
+	}
+}
